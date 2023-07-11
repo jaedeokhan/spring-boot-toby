ed6f54ac2ec7954d86c0b764a80c5883f6cda424
jaedeokhan <hjaedeok@gmail.com>
2023-07-11 23:46:54 +0900

    feat : 애노테이션 매핑 정보 사용 - Spring mvc Controller에서 String을 반환하면 DispatcherSerlvet에   ViewResolver가 prefix, postfix를 붙여서 화면을 반환해주는데 까먹고   있었다...ㅎ 404가 계속 났는데 왜 발생하고 있었는지 몰랐다.

--- helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,26 +1,10 @@
 package tobyspring.helloboot;
 
-import org.springframework.boot.ApplicationContextFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class HellobootApplication {
 
